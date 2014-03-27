8ea8eed876627c60215cc8487088ca2638f4700c
Dan Leehr <dan.leehr@nescent.org>
2014-03-27 17:07:49 -0400

    Add spring controller to check if doi.db file is available

--- dspace/modules/xmlui/src/main/java/org/dspace/springmvc/DoiDBCheck.java
+++ dspace/modules/xmlui/src/main/java/org/dspace/springmvc/DoiDBCheck.java
@@ -25,12 +25,11 @@ public class DoiDBCheck {
             // Attempts a simple write operation to see if the DOI file is usable
             writer = response.getWriter();
             Minter myMinter = new Minter();
-            // Minter does not instantiate a DOIDatabase until calling getKnownDOI()
+            // getKnownDOI instantiates the DOIDatabase
             DOI doi = myMinter.getKnownDOI(DUMMY_DOI_STRING);
             DOI dummyDoi = new DOI(DUMMY_DOI_STRING, DOI.Type.TOMBSTONE);
-            // Mint a DOI then remove it
-            myMinter.mintDOI(dummyDoi); // If DOI is already in DB this does not write
-            myMinter.remove(dummyDoi); // This writes
+            // mintDOI is the only exposed write operation
+            myMinter.mintDOI(dummyDoi);
             writer.println("OK");
         } catch (Exception ex) {
             writer.println("Exception: " + ex);
