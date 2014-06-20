04718e5fc078eccb2f953854a5d28acaca11efef
Nathan Day <rnathanday@yahoo.com>
2014-06-20 23:26:47 +0000

    do not delete checked authors/keywords unless "remove selected" button clicked
    The org.dspace.submit.step.getRepeatedParameter() method previously filtered out
    any checked items for an list that permits removal (for example, an author or
    keyword list on the page used to describe publication by inputting metadata),
    regardless of the user's intention to add a new item or remove existing items
    from that list.
    
    This commit updates the filter by only filtering out checked items if the button
    clicked to initiate the Submit request is a "Remove" button for the given
    metadata item, per the button's input/name value.

--- dspace/modules/api/src/main/java/org/dspace/submit/step/DescribePublicationStep.java
+++ dspace/modules/api/src/main/java/org/dspace/submit/step/DescribePublicationStep.java
@@ -1112,7 +1112,7 @@ public class DescribePublicationStep extends AbstractProcessingStep {
 
         int i = 1;    //start index at the first of the previously entered values
         boolean foundLast = false;
-        String buttonPressed = Util.getSubmitButton(request, "");
+        String buttonPressed = Util.getSubmitButton(request, null);
 
         // Iterate through the values in the form.
         while (!foundLast)
@@ -1144,7 +1144,8 @@ public class DescribePublicationStep extends AbstractProcessingStep {
                 String[] selected = request.getParameterValues(metadataField
                         + "_selected");
 
-                if (selected != null && buttonPressed.equals("submit_" + metadataField + "_delete"))
+                if (   selected != null && buttonPressed != null 
+                    && buttonPressed.equals("submit_" + metadataField + "_delete"))
                 {
                     for (String aSelected : selected) {
                         if (aSelected.equals(metadataField + "_" + i)) {
